7735f466ef3d69d36879730a547ff66ebaa3572a
xankarawa@gmail.com <xankarawa@gmail.com>
2021-01-14 22:12:42 +0300

    123

---
+++ Market/Models/WatchContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Market.Models
+{
+    public class WatchContext : DbContext
+    {
+        public DbSet<Watch> Watches { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+
+    }
+}
